59d3f8a2817ff81e3b992b2975de364a8439a274
Vohai2003 <vophanphuochai2003@gmail.com>
2023-04-26 19:43:59 +0700

    Editing script.js

--- logic/script.js
+++ logic/script.js
@@ -15,9 +15,26 @@ var $fen = $('#fen')
 var $pgn = $('#pgn')
 var whiteSquareGrey = '#bec8d1'
 var blackSquareGrey = '#373b3e'
-
-
-
+import * as Colyseus from "../libary/colyseus/colyseus.js"
+var connectionUrl = "ws://"+ location.host + ":8000"
+var client = new Colyseus.Client(connectionUrl)
+var default_room = client.join("public_hall")
+var username = $('#username')
+default_room.onJoin.add(function(){
+  default_room.send({username: username})
+})
+default_room.onMessage.add(function(message){
+  if (message["tag"] == "welcome") {
+  amIspectator = message["spectator"]
+  myside = message["myside"]
+  connectionReady = true
+  }
+})
+var myside = 'w'
+var amIspectator = false
+var connectionReady;
+while (typeof connectionReady == "undefined") {
+}
 function removeGreySquares () {
   $('#myBoard .square-55d63').css('background', '')
 }
@@ -36,10 +53,10 @@ function greySquare (square) {
 function onDragStart (source, piece, position, orientation) {
   // do not pick up pieces if the game is over
   if (game.game_over()) return false
-
+  // do not pick the pieces if the player is spectator
+  if (amIspectator) return false
   // only pick up pieces for the side to move
-  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+  if (game.turn !== myside || piece.search(myside)===-1) {
     return false
   }
 }
@@ -55,7 +72,11 @@ function onDrop (source, target) {
 
   // illegal move
   if (move === null) return 'snapback'
+  //leave this part for piece update
+  if (game.turn === myside && !amIspectator) {
 
+  }
+  //leave this part for promotion button group
   updateStatus()
 }
 
